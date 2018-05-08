2a1545cf3eddd718198b084259eef406de7588f8
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-05-07 23:30:44 -0400

    clones

--- src/clojars_download/core.clj
+++ src/clojars_download/core.clj
@@ -2,7 +2,9 @@
   (:require [clojure.java.io :as io]
             [clojure.pprint :as pp]))
 
-(def feed "feed.clj")
+(def feed-file "feed.clj")
+(def new-feed-file "new-feeds.clj")
+(def clones-file "clones.txt")
 
 (defn good-project-clj? [{:keys [scm] :as m}]
   (when-let [url (not-empty (:url scm))]
@@ -14,22 +16,25 @@
           (catch java.io.FileNotFoundException _
             false))))))
 
-(defn download []
-  (with-open [f (io/reader feed)]
+(defn new-feeds []
+  (with-open [f (io/reader feed-file)]
     (let [rs (map read-string (line-seq f))
           projects (->> rs
                         #_(take 10)
-                        (filter (fn [{:keys [^String artifact-id ^String description scm versions] :as m}]
+                        (filter (fn [{:keys [group-id ^String artifact-id ^String description scm versions] :as m}]
                              (and scm
                                   (string? description)
                                   (not= description "FIXME: write description")
                                   (not (.contains (.toLowerCase description) "deprecated"))
                                   (not (.contains (.toLowerCase description) "clojurescript"))
+                                  (not (.contains (.toLowerCase description) "clojure[script]"))
+                                  (not (.contains (.toLowerCase description) "cljs"))
                                   (not
                                     (and (or (.contains (.toLowerCase description) "leiningen")
                                              (.contains (.toLowerCase description) "lein")
                                              (.contains (.toLowerCase description) "boot"))
                                          (.contains (.toLowerCase description) "plugin")))
+                                  (not= "cljsjs" group-id)
                                   (not (.contains (.toLowerCase artifact-id) "lein-"))
                                   (not (.contains (.toLowerCase artifact-id) "-lein"))
                                   (not (.contains (.toLowerCase artifact-id) "boot-"))
@@ -41,19 +46,35 @@
                                   #_(some (fn [^String s]
                                           (not (.endsWith s "SNAPSHOT")))
                                         versions)
+                                  (string? (:url scm))
                                   (not= "" (:url scm))
+                                  (not= "https://github.com/cljsjs/packages" (:url scm))
                                   (not= "HEAD" (:tag scm))
                                   #_(good-project-clj? m)))))
           projects (vec projects)]
       #_(vec (take 10 projects))
       #_(prn (count projects) "projects")
-      (spit "new-feeds.clj"
+      (spit new-feed-file
             (with-out-str
               (mapv prn projects)))
       projects
       )))
 
+(defn clone-projects [feeds]
+  (let [clone-cmds (->> feeds
+                        (keep (fn [{:keys [scm]}]
+                                (let [^String url (:url scm)]
+                                  (when-let [tag (not-empty (:tag scm))]
+                                    (when (string? url)
+                                      (let [[match? group artifact] (re-matches #"https://github.com/(\w+)/(\w+)" url)]
+                                        (when match?
+                                          (let [folder (str group "::" artifact)]
+                                            (str "git_clone " url " " folder " " tag))))))))))]
+    (spit clones-file
+          (with-out-str
+            (mapv println clone-cmds)))))
+
 (comment
-        (def fst (first (download)))
-        (good-project-clj? fst)
+        (new-feeds)
+        (clone-projects (new-feeds))
         )
